7f511fce748398c07be3f9e3af30f11bc07ab150
Ubuntu <bitnami@ip-172-31-0-226.(none)>
2014-05-10 07:58:04 +0000

    Task D: Cart Creation.

--- depot/app/models/product.rb
+++ depot/app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
@@ -9,5 +12,16 @@ class Product < ActiveRecord::Base
 
   def self.latest
     Product.order(:updated_at).last
+  end
+
+  private
+    # Ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+      if line_item.empty?
+        return true
+      else
+        errors.add(:base, 'Line Item present')
+        return false
+      end
   end
 end
